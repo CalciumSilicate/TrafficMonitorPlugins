301b5f55028f8bc8db4595396827db22fba87a45
CalciumSilicate <calcium_silicate@163.com>
2026-06-04 17:29:35 +0800

    fix(AMPManager): 调整异常状态闪烁逻辑

--- Plugins/AMPManager/AMPManagerItem.cpp
+++ Plugins/AMPManager/AMPManagerItem.cpp
@@ -1,5 +1,16 @@
 #include "pch.h"
 #include "AMPManagerItem.h"
+#include <algorithm>
+
+namespace
+{
+    constexpr DWORD ErrorBlinkIntervalMs = 200;
+
+    bool IsErrorStatus(const std::wstring& value)
+    {
+        return value == L"ERR" || value == L"FATAL";
+    }
+}
 
 CAMPManagerItem::CAMPManagerItem(AmpMetricId id)
     : m_metric_id(id)
@@ -41,4 +52,56 @@ const wchar_t* CAMPManagerItem::GetItemValueText() const
 const wchar_t* CAMPManagerItem::GetItemValueSampleText() const
 {
     return g_data.GetMetricDefinition(m_metric_id).sample;
+}
+
+bool CAMPManagerItem::IsCustomDraw() const
+{
+    return m_metric_id == AmpMetricId::Status;
+}
+
+int CAMPManagerItem::GetItemWidthEx(void* hDC) const
+{
+    if (!IsCustomDraw())
+        return 0;
+
+    CDC* pDC = CDC::FromHandle((HDC)hDC);
+    CString label(GetItemLableText());
+    CString value(GetItemValueText());
+    CString sample(GetItemValueSampleText());
+
+    int value_width = (std::max)(pDC->GetTextExtent(value).cx, pDC->GetTextExtent(sample).cx);
+    return pDC->GetTextExtent(label).cx + g_data.DPI(6) + value_width;
+}
+
+void CAMPManagerItem::DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode)
+{
+    if (!IsCustomDraw())
+        return;
+
+    CDC* pDC = CDC::FromHandle((HDC)hDC);
+    CRect rect(CPoint(x, y), CSize(w, h));
+    CRect rc_label{ rect };
+    CRect rc_value{ rect };
+
+    CString label(GetItemLableText());
+    std::wstring value(GetItemValueText());
+    rc_label.right = rc_label.left + pDC->GetTextExtent(label).cx;
+    rc_value.left = rc_label.right + g_data.DPI(6);
+
+    COLORREF default_color = dark_mode ? RGB(255, 255, 255) : RGB(0, 0, 0);
+    pDC->SetTextColor(default_color);
+    pDC->DrawText(label, rc_label, DT_VCENTER | DT_SINGLELINE | DT_NOPREFIX);
+
+    if (IsErrorStatus(value))
+    {
+        bool blink_on = (GetTickCount() / ErrorBlinkIntervalMs) % 2 == 0;
+        COLORREF error_color = dark_mode ? RGB(255, 121, 120) : RGB(195, 0, 0);
+        pDC->SetTextColor(blink_on ? error_color : default_color);
+    }
+    else
+    {
+        pDC->SetTextColor(default_color);
+    }
+
+    pDC->DrawText(value.c_str(), rc_value, DT_VCENTER | DT_SINGLELINE | DT_NOPREFIX);
 }
--- Plugins/AMPManager/AMPManagerItem.h
+++ Plugins/AMPManager/AMPManagerItem.h
@@ -17,6 +17,9 @@ public:
     virtual const wchar_t* GetItemLableText() const override;
     virtual const wchar_t* GetItemValueText() const override;
     virtual const wchar_t* GetItemValueSampleText() const override;
+    virtual bool IsCustomDraw() const override;
+    virtual int GetItemWidthEx(void* hDC) const override;
+    virtual void DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode) override;
 
 private:
     AmpMetricId m_metric_id{ AmpMetricId::Status };
